ea80731cb0319a5a847eb727ab87b4a4227ef252
Milton Chandradas <milton.chandradas@live.com>
2022-01-24 17:03:14 -0500

    REST API...

--- server.js
+++ server.js
@@ -1,8 +1,43 @@
-const { getNextWord} = require("./utils");
+const { getNextWord } = require("./utils");
+const express = require("express");
+const app = express();
 
-let excludeLetters = [];
-let includeLetters = [];
-let exactMatches = [];
-let notExactMatches = [];
+const PORT = process.env.PORT || 3000;
 
-console.log(getNextWord(excludeLetters, includeLetters, exactMatches, notExactMatches));
+app.get("/", (req, res) => {
+   let excludeLetters = req.query.exclude ? req.query.exclude.split(",") : [];
+   let includePattern = req.query.include ? req.query.include.split(",") : [];
+   let includeLetters = [];
+   let exactMatches = [];
+   let notExactMatches = [];
+
+   includePattern.forEach((pattern) => {
+      let matches = pattern.split("-");
+
+      if (matches[0] && matches[1] && matches[2]) {
+         includeLetters.push(matches[0]);
+         notExactMatches.push({
+            value: matches[0],
+            position: matches[1],
+         });
+      } else if (matches[0] && matches[1]) {
+         includeLetters.push(matches[0]);
+         exactMatches.push({
+            value: matches[0],
+            position: matches[1],
+         });
+      }
+   });
+
+   let nextWord = getNextWord(
+      excludeLetters,
+      includeLetters,
+      exactMatches,
+      notExactMatches
+   );
+   res.status(201).json({ nextWord });
+});
+
+app.listen(PORT, () => {
+   console.log(`Listening on port ${PORT}`);
+});
